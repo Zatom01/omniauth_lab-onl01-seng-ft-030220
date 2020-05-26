73860e70576c0f8043a2c7f0dfdbbad85ab5357d
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-26 19:02:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'welcome/home'
   # Add your routes here
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  root "welcome#home"
 end
